0f1fc4049742ad6a79fc9b67f8dbdb391d51ecef
KarriSarmanne <karri.sarmanne@edu.salpaus.fi>
2020-12-06 22:10:42 +0000

    20201206

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+const nimi = "Karri Sarmanne"
+let ika = 16
+
+
+var opiskelu = true;
+
+if (opiskelu = true){
+  const opiskelu = "opiskelija"
+}else{
+  const opiskelu = "ammattilainen"
+};
+
+const surffaajalle = `Hei sivuilleni surffaaja, olen ${nimi} ja olen ${ika} vuotias ${opiskelu}`;
+
+console.log(surffaajalle);
+
