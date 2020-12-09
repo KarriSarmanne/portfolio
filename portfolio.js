7dd81f103b73df60a1b50373f842cc2d1a4ad8a9
KarriSarmanne <karri.sarmanne@edu.salpaus.fi>
2020-12-09 17:33:17 +0000

    20201209

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,65 @@
-const nimi = "Karri Sarmanne"
-let ika = 16
+const nimi = "Karri Sarmanne";
+let ika = 16;
+let opiskelija = true;
 
-
-const opiskelu;
-
-if (opiskelu = true){
-  const opiskelu = "opiskelija"
+if(opiskelija == true){
+  let opiskelija = "opiskelija";
+  const surffaajalle = `Hei sivuilleni surffaaja, olen ${nimi} ja olen ${ika} vuotias ${opiskelija}`;
+  console.log(surffaajalle);
 }else{
-  const opiskelu = "ammattilainen"
+  let opiskelija = "ammattilainen";
+  const surffaajalle = `Hei sivuilleni surffaaja, olen ${nimi} ja olen ${ika} vuotias ${opiskelija}`;
+  console.log(surffaajalle);
 };
 
-const surffaajalle = `Hei sivuilleni surffaaja, olen ${nimi} ja olen ${ika} vuotias ${opiskelu}`;
+const ystavat = ['Roope Pesonen', 'Leo Toivonen', 'Antero Ruban', 'Timi Lukumies'];
+console.log('1. ystäväni: ' + ystavat[0].split(' ')[0] + ' ' + ystavat[0].split(' ')[1]);
+console.log('2. ystäväni: ' + ystavat[1].split(' ')[0] + ' ' + ystavat[1].split(' ')[1]);
+console.log('3. ystäväni: ' + ystavat[2].split(' ')[0] + ' ' + ystavat[2].split(' ')[1]);
+console.log('4. ystäväni: ' + ystavat[3].split(' ')[0] + ' ' + ystavat[3].split(' ')[1]);
 
-console.log(surffaajalle);
+let taysiIkainen;
+const perhe = [
+  {
+    id: 1,
+    etunimi: 'Karri',
+    ika: 16,
+    taysiIkainen: false
+  },
+  {
+    id: 2,
+    etunimi: 'Kiia',
+    ika: 14,
+    taysiIkainen: false
+  },
+  {
+    id: 3,
+    etunimi: 'Kaisla',
+    ika: 8,
+    taysiIkainen: false
+  },
+  {
+    id: 4,
+    etunimi: 'Antti',
+    ika: 47,
+    taysiIkainen: true
+  },
+  {
+    id: 5,
+    etunimi: 'Anne',
+    ika: 43,
+    taysiIkainen: true
+  },
+];
 
+let taysiIkaiset;
+let alaikaiset;
+const perheenjasenet = perhe.map(function(perheenjasen){
+  if(perheenjasen.taysiIkainen == true){
+    let taysiIkaiset = perheenjasen.etunimi
+    console.log("Täysi-ikäiset perheenjäseneni: " + taysiIkaiset)
+  }else{
+    let alaikaiset = perheenjasen.etunimi
+    console.log("Alaikäiset perheenjäseneni: " + alaikaiset)
+  }
+});
