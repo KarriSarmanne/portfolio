5814d6030a6d6e26966b772177e20be538c0239b
KarriSarmanne <karri.sarmanne@edu.salpaus.fi>
2020-12-17 14:55:39 +0000

    20201217

--- json.js
+++ json.js
@@ -15,16 +15,16 @@ var getJSON = function(url, callback){
 };
 
 getJSON('https://api.apify.com/v2/datasets/BDEAOLx0DzEW91s5L/items?format=json&clean=1', function(err, data){
-  let taulukko = `<table width="100%" style="border=1px solid black">`
+  let taulukko = `<table width="100%" style="border=1px solid black"><tr><td>Pvm.</td><td>Tartunnat</td></tr>`
   const historia = data.map(function(paiva){
     if(paiva.confirmedCases != undefined){
-      taulukko = taulukko + `<tr><td>${paiva.confirmedCases}</td></tr>`;
+      taulukko = taulukko + `<tr><td>${paiva.lastUpdatedAtApify}</td><td>${paiva.confirmedCases}</td></tr>`;
     }else if(paiva.testedCases != undefined){
-      taulukko = taulukko + `<tr><td>${paiva.testedCases}</td></tr>`;
+      taulukko = taulukko + `<tr><td>${paiva.lastUpdatedAtApify}</td><td>${paiva.testedCases}</td></tr>`;
     }else if(paiva.infected != undefined){
-      taulukko = taulukko + `<tr><td>${paiva.infected}</td></tr>`;
+      taulukko = taulukko + `<tr><td>${paiva.lastUpdatedAtApify}</td><td>${paiva.infected}</td></tr>`;
     }else{
-      taulukko = taulukko + `<tr><td>Ei dataa</td></tr>`;
+      taulukko = taulukko + `<tr><td>${paiva.lastUpdatedAtApify}</td><td>Ei dataa</td></tr>`;
     }
   });
   taulukko = taulukko + `</table>`;
